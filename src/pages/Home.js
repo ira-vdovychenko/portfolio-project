ca40c6495389c7c0c2ad685026f3a08e13b6a590
Ira <vdovychenko.ire@gmail.com>
2023-07-01 13:51:26 +0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import Header from "../components/header/Header";
+
+const Home = () => {
+    return ( 
+        <>
+            <Header />
+            <main className="section">
+                <div className="container">
+                    <ul className="content-list">
+                        <li className="content-list__item">
+                        <h2 className="title-2">Frontend</h2>
+                        <p>JavaScript, ReactJS, Redux, HTML, CSS, NPM, BootStrap, Yarn, StyledComponents</p>
+                        </li>
+                    </ul>
+                </div>
+            </main>
+
+        </>
+     );
+}
+ 
+export default Home;
